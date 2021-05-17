9084ada197b79e04efd349c2ba840a9756f4f1cb
rockyFierro <finnslighthouse@gmail.com>
2021-05-16 22:09:42 -0700

    useForm > stateful logic is handled by a custom hook | arf

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,21 @@
-// write your custom hook here to control your checkout form
+import { useState } from "react"
+
+export const useForm = (initialValues) => {
+  const [ values, setValues ] = useState(initialValues);
+  const [ showSuccessMessage, setShowSuccessMessage ] = useState ( false );
+
+  const handleChanges = event => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
+    })
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setShowSuccessMessage(true);
+  }
+
+  return { values, setValues }
+
+}
